38f05c413eb84bd73ce48de2f1cb7c25d730ae9c
Andrew Wittrock <andy@wittrock.us>
2025-02-02 15:44:35 -0600

    Add account menu

--- components/HeaderMenu/HeaderMenu.component.tsx
+++ components/HeaderMenu/HeaderMenu.component.tsx
@@ -1,7 +1,11 @@
 import { Menu } from "antd";
 import { HeaderMenuProps } from "./HeaderMenu.d";
 import styles from './HeaderMenu.module.scss';
+import { AccountMenu } from "@uac/components/AccountMenu";
 
-export const HeaderMenuComponent = ({items, onClick}:HeaderMenuProps) =>
+export const HeaderMenuComponent = ({items, onClick}:HeaderMenuProps) => <>
+    <div className={styles.accountMenu}>
+        <AccountMenu />
+    </div>
     <Menu mode="horizontal" className={styles.menu} items={items} onClick={onClick}/>
-;
+</>;
